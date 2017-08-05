defaa29642c6cddf38a3d12c91730fa6dd0dddb7
VRDragon <30727840+VRDragon@users.noreply.github.com>
2017-08-05 16:46:43 +0200

    update
    upgrade

--- gpio.php
+++ gpio.php
@@ -6,7 +6,23 @@ if (isset ( $_GET["pic"] )) {
 	$pic = strip_tags ($_GET["pic"]);
 	
 	//test if value is a number
-	if ( (is_numeric($pic)) && ($pic <= 23) && ($pic >= 0) ) {
+	if ( (is_numeric($pic)) && ($pic <= 16) && ($pic >= 0) ) {
+		
+		//set the gpio's mode to output		
+		system("gpio mode ".$pic." out");
+		//reading pin's status
+		exec ("gpio read ".$pic, $status, $return );
+		//set the gpio to high/low
+		if ($status[0] == "0" ) { $status[0] = "1"; }
+		else if ($status[0] == "1" ) { $status[0] = "0"; }
+		system("gpio write ".$pic." ".$status[0] );
+		//reading pin's status
+		exec ("gpio read ".$pic, $status, $return );
+		//print it to the client on the response
+		echo($status[0]);
+		
+	}
+	if ( (is_numeric($pic)) && ($pic <= 27) && ($pic >= 21) ) {
 		
 		//set the gpio's mode to output		
 		system("gpio mode ".$pic." out");
--- index.php
+++ index.php
@@ -6,26 +6,38 @@
         <meta charset="utf-8" />
         <title>FORLED PANEL</title>
     </head>
-	<H1>
-       FORLED PANEL
-    </H1>
+ 
     <body style="background-color: black;">
     <!-- On/Off button's picture -->
 	<?php
 	$page = $_SERVER['PHP_SELF'];
 	$sec = "10";
 	header("Refresh: $sec; url=$page");
-	$val_array = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
-	$vystup = array(0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,21,22,23,24,25,26,27,28,29,30,31);
+	$val_array = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 	//this php script generate the first page in function of the file
 	for ( $i= 0; $i<17; $i++) {
 		//set the pin's mode to output and read them
-		system("gpio mode ".$vystup[$i]." out");
-		exec ("gpio read ".$vystup[$i], $val_array[$i], $return );
+		system("gpio mode ".$i." out");
+		exec ("gpio read ".$i, $val_array[$i], $return );
+	}
+	for ( $i= 21; $i<28; $i++) {
+		//set the pin's mode to output and read them
+		system("gpio mode ".$i." out");
+		exec ("gpio read ".$i, $val_array[$i], $return );
 	}
 	//for loop to read the value
 	$i =0;
-	for ($i = 0; $i < 24; $i++) {
+	for ($i = 0; $i < 17; $i++) {
+		//if off
+		if ($val_array[$i][0] == 0 ) {
+			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' onclick='change_pin (".$i.");'/>");
+		}
+		//if on
+		if ($val_array[$i][0] == 1 ) {
+			echo ("<img id='button_".$i."' src='data/img/green/green_".$i.".jpg' onclick='change_pin (".$i.");'/>");
+		}	 
+	}
+	for ($i = 21; $i < 28; $i++) {
 		//if off
 		if ($val_array[$i][0] == 0 ) {
 			echo ("<img id='button_".$i."' src='data/img/red/red_".$i.".jpg' onclick='change_pin (".$i.");'/>");
